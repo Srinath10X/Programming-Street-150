c33e8767bbce160010fe39e9aeed403d65e347e2
Srinath10X <srinathcreates@gmail.com>
2024-11-13 23:42:28 +0530

    feat: added 02-primeornot program

---
+++ PS-Sprint-1/02-primeornot.cpp
@@ -0,0 +1,22 @@
+#include <cmath>
+
+bool isPrime(unsigned int n) {
+  //
+  if (n <= 1) {
+    return false;
+  }
+
+  if (n == 2) {
+    return true;
+  }
+  if (n % 2 == 0) {
+    return false;
+  }
+
+  for (unsigned int i = 3; i <= sqrt(n); i += 2) {
+    if (n % i == 0) {
+      return false;
+    }
+  }
+  return true;
+}
